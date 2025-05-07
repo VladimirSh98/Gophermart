d9556924f07a8fdda61b9a2514c5c77ff579907a
Vladimir <V.Shpichko@mail.ru>
2025-05-07 13:57:21 +0300

    fix accrual client

--- internal/app/client/accrual/get_by_number.go
+++ internal/app/client/accrual/get_by_number.go
@@ -7,17 +7,17 @@ import (
 )
 
 func (c *HTTPClient) GetByNumber(number string) (*Calculations, error) {
+	var data Calculations
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/orders/%s", c.baseURL, number), nil)
 	if err != nil {
-		return nil, err
+		return &data, err
 	}
 	var resp *http.Response
 	resp, err = c.httpClient.Do(req)
 	if err != nil {
-		return nil, err
+		return &data, err
 	}
 	defer resp.Body.Close()
-	var data Calculations
 	data.StatusCode = resp.StatusCode
 	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
 		return nil, err
