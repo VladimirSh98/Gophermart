2bf583b0a833d0e52fd6ef103b282a923d61275b
Vladimir <V.Shpichko@mail.ru>
2025-05-17 14:59:33 +0300

    add something

--- internal/app/repository/user/create.go
+++ internal/app/repository/user/create.go
@@ -3,7 +3,7 @@ package user
 import "context"
 
 func (repo *Repository) Create(ctx context.Context, login string, password string) (int, error) {
-	query := "INSERT INTO \"user\" (login, password, archived) VALUES ($1, $2, false) RETURNING id;"
+	query := "INSERT INTO \"user\" (login, hash, archived) VALUES ($1, $2, false) RETURNING id;"
 	var ID int
 	err := repo.Conn.QueryRow(query, login, password).Scan(&ID)
 
--- migrations/20250501161053_initial.sql
+++ migrations/20250501161053_initial.sql
@@ -4,7 +4,7 @@ CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     created_at timestamp default NOW(),
     login text,
-    password text,
+    hash text,
     archived bool
 );
 CREATE UNIQUE INDEX IF NOT EXISTS user_login_udx ON "user"(login) WHERE archived is false;
